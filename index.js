edd7bf2912c93af581ba33bf84439a38a4161bd3
halldwang <ahthw@hotmail.com>
2020-01-13 23:14:39 +0800

    for gh-pages

--- index.js
+++
@@ -1,4 +0,0 @@
-import BeeRouter from "./components/BeeRouter";
-import BeeRouterEmitter from "./components/Emitter";
-
-export { BeeRouter, BeeRouterEmitter };
---
+++ precache-manifest.b17b8cf833e142cf1e77f9486a115e7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b24ea9d3e62093be7544718af0ec7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8ae57aaa76047e31ec3",
+    "url": "/static/css/main.6a6f24b6.chunk.css"
+  },
+  {
+    "revision": "ca76837eb57548259821",
+    "url": "/static/js/2.bbd86fd0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bbd86fd0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8ae57aaa76047e31ec3",
+    "url": "/static/js/main.b52824cd.chunk.js"
+  },
+  {
+    "revision": "696942293a92ac3a2bb6",
+    "url": "/static/js/runtime-main.889ad4b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
